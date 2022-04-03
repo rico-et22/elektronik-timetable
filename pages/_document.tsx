9737be0cf042c7a3d6ddf532e4e0af1c8ac36957
Kamil Pawlak <ricoet22@gmail.com>
2022-04-03 12:42:47 +0200

    feat: add favicon & theme-color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,26 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pl" className="text-neutral-800">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="theme-color" content="#D4293D" />
+        </Head>
         <body>
           <Main />
           <NextScript />
