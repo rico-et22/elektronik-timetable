ca09237282eb1d939a32c18165840ba8c5dd4bd5
Kamil Pawlak <ricoet22@gmail.com>
2022-04-18 23:09:36 +0200

    feat: remember the last opened page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,17 +27,63 @@ const Layout = ({
 }: Props) => {
   const router = useRouter();
   const { desktopComponent, showSpinner } = React.useContext(SettingsContext);
-  const desktopLayoutRef = React.useRef<HTMLDivElement>(null)
+  const desktopLayoutRef = React.useRef<HTMLDivElement>(null);
 
+  const lastPathKey = "elektronik-timetable-last-path";
+
+  // Get last opened path saved in localStorage.
+  // If the path corresponds to a valid class/teacher/room in timeTableList, redirect to it.
+  // Otherwise, if possible, redirect to the first class in the list.
+  // This code runs only on index page "/".
   React.useEffect(() => {
-    if (timeTableList.classes.length > 0 && router.asPath === "/") {
-      router.push(`/class/${timeTableList.classes[0].value}`);
+    const lastPath = window.localStorage.getItem(lastPathKey);
+    if (router.asPath === "/") {
+      let isCorrect = false;
+      const parsedPath: string = lastPath ? JSON.parse(lastPath) : "";
+      if (typeof window !== "undefined" && lastPath) {
+        const splittedPath = parsedPath.split("/");
+        if (
+          (splittedPath[1] === "class" &&
+            timeTableList.classes.find(
+              (singleClass) => singleClass.value === splittedPath[2]
+            )) ||
+          (splittedPath[1] === "teacher" &&
+            timeTableList.teachers?.find(
+              (singleTeacher) => singleTeacher.value === splittedPath[2]
+            )) ||
+          (splittedPath[1] === "room" &&
+            timeTableList.rooms?.find(
+              (singleRoom) => singleRoom.value === splittedPath[2]
+            ))
+        ) {
+          isCorrect = true;
+        }
+      }
+      if (isCorrect) router.push(parsedPath);
+      else if (timeTableList.classes.length > 0) {
+        router.push(`/class/${timeTableList.classes[0].value}`);
+      }
     }
-  }, [router, timeTableList.classes]);
+  }, [
+    router,
+    timeTableList.classes,
+    timeTableList.rooms,
+    timeTableList.teachers,
+  ]);
+
+  // If the timetable is valid, save its path to localStorage.
+  // The app will automatically redirect to it when opening the app from the index page - if the path is still valid in timeTableList.
+  React.useEffect(() => {
+    if (router.asPath) {
+      if (typeof window !== "undefined" && timeTableStatus === "ok") {
+        window.localStorage.setItem(lastPathKey, JSON.stringify(router.asPath));
+      }
+    }
+  }, [router.asPath, timeTableStatus]);
 
   React.useEffect(() => {
-    if (showSpinner) desktopLayoutRef.current?.scrollTo(0,0)
-  }, [showSpinner])
+    if (showSpinner) desktopLayoutRef.current?.scrollTo(0, 0);
+  }, [showSpinner]);
 
   return (
     <>
@@ -49,9 +95,7 @@ const Layout = ({
         <HeaderBar />
         <div
           className={`relative min-h-[calc(100vh-4.5rem)] ${
-            showSpinner
-              ? "h-[calc(100vh-9.75rem)] overflow-hidden"
-              : ""
+            showSpinner ? "h-[calc(100vh-9.75rem)] overflow-hidden" : ""
           }`}
         >
           {timeTable && (
