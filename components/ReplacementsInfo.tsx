3bb87bb904b7a0ce8860e7444628de88c3bde48e
Olek Baran <olekbaran24@gmail.com>
2023-09-24 11:47:38 +0200

    feat: better replacements info (#29)
    * feat: better replacements info
    
    * chore: refactoring

--- components/ReplacementsInfo.tsx
+++ components/ReplacementsInfo.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 
-const ReplacementsInfo = ({ date }: { date: string }) => (
-  <div className="px-4 lg:px-10 mt-6 mb-4 flex justify-between text-sm sm:text-base">
-    <p>Dotyczy dnia: {date}</p>
+const ReplacementsInfo = ({ date }: { date: Date }) => (
+  <div className="px-4 lg:px-10 mt-6 mb-4 flex justify-between gap-3 text-sm sm:text-base">
+    <p>
+      Dzień: {date.toLocaleDateString('pl-PL')} (
+      {date.toLocaleDateString('pl-PL', { weekday: 'long' })})
+    </p>
     <p>
       Dane z{' '}
       <a
